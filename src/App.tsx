d6076f3e4b82349ea14639ae38f18e98e605ccb8
Bipin <bipinyadav9769@gmail.com>
2025-03-17 21:57:42 +0530

    feat: enhance Input component to support ref forwarding and add new input field in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import Input from './components/Input'
 import { Button } from './components/Button.tsx';
 import Container from './components/Container.tsx';
 
 function App() {
-
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
       {/* <Container as={Button} /> */}
-      <Container as={Button}>Click Me </Container> 
+      {/* <Container as={Button}>Click Me </Container>  */}
       {/* <p>
         <Button >Click me</Button>
       </p>
@@ -17,6 +17,7 @@ function App() {
       </p>
     <Input label="Name" id="name" type="text" />
     <Input label="Age" id="age" type="number" /> */}
+    <Input label="Test" id="test" ref={input} />
     </main>
   )
 }
