9b00e1ab8c3154336e98d1ed6db0a488bdecdd5f
Bipin <bipinyadav9769@gmail.com>
2025-03-17 20:56:05 +0530

    feat: added Button component and integrated into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import Input from './components/Input'
+import Button from './components/Button.tsx';
 
 function App() {
 
   return (
-    <>
+    <main>
+      <p>
+        <Button el='button'>Click me</Button>
+      </p>
+      <p>
+        <Button el="anchor" href='https://google.com'>A Link</Button>
+      </p>
     <Input label="Name" id="name" type="text" />
     <Input label="Age" id="age" type="number" />
-    </>
+    </main>
   )
 }
 
