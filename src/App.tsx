a8d8ea347b62b9eed6f817471e8ba3fd5809f524
Bipin <bipinyadav9769@gmail.com>
2025-03-17 21:34:21 +0530

    basic polymorphic component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import Input from './components/Input'
 import { Button } from './components/Button.tsx';
+import Container from './components/Container.tsx';
 
 function App() {
 
   return (
     <main>
-      <p>
+      <Container as={Button} />
+      {/* <p>
         <Button >Click me</Button>
       </p>
       <p>
         <Button  href='https://google.com' target={"_blank"}>A Link</Button>
       </p>
     <Input label="Name" id="name" type="text" />
-    <Input label="Age" id="age" type="number" />
+    <Input label="Age" id="age" type="number" /> */}
     </main>
   )
 }
